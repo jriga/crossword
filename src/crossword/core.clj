802c5cf470a54b927896a220a9f444a165a7f2b6
Jerome Riga <jriga@zemis.co.uk>
2020-10-26 16:23:14 +0000

    basic crossword

--- src/crossword/core.clj
+++ src/crossword/core.clj
@@ -1,7 +1,8 @@
 (ns crossword.core
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.set :as s]))
 
-(declare init-board random-start find-space add-word)
+(declare init-board random-start filled? find-coord can-place find-space add-word)
 
 (defonce default-options
   {:max-length 7
@@ -11,6 +12,8 @@
 (defonce ^:dynamic *options* (merge default-options {}))
 
 (defonce empty-char \.)
+
+(defrecord Word [direction coord pattern])
 
 (defn words
   "Creates subset of words that match required criterias
@@ -46,31 +49,68 @@
         {:words []
          :grid (mapv #(into [] %) (partition n (repeat (* n n) empty-char)))})))
 
-(defrecord Word [direction coord pattern])
-
-(defn- filled?
-  "Predicate than tells if a board is filled"
-  [board]
-  )
+(def ^:private not-nil? (complement nil?))
+(defn- add-word [board [dir [x y]] word]
+  (when (and (not-nil? dir) (not-nil? x) (not-nil? y))
+    (let [coord #(if (= 0 dir) [y (+ x %)] [(+ y %) x])]
+      (swap! board (fn [b]
+                     {:words (conj (b :words) (Word. dir [x y] word))
+                      :grid (reduce #(update-in %1 (coord (first %2)) (constantly (last %2)))
+                                    (b :grid)
+                                    (map-indexed vector word))})))))
 
 (defn fill-board
   "Fills in given board with the words provided"
-  [board word-seq]
-  (let [sorted-word (sort-by count > word-seq)
-        _ (add-word board (random-start board) (first sorted-word))]
-    ))
+  ([board word-seq] (fill-board board word-seq (random-start board)))
+  ([board word-seq position]
+   (let [sorted-word (sort-by count > word-seq)
+         _ (add-word board position (first sorted-word))]
+     (doseq [w (drop 1 sorted-word)]
+       (when-let [intersections (s/intersection (set w) (set (:pattern (last (:words @board)))))]
+         (println "Current word: " w "intersections with " (:pattern (last (:words @board))) "is " intersections)
+         (add-word board
+                   (first (map  #(-> %
+                                     (find-coord (last (:words @board)))
+                                     (can-place board w)
+                                     (first))
+                                intersections))
+                   w))))))
 
-(defn generate [user-options]
-  (binding [*options* (merge default-options user-options)]
-    (fill-board (create-board) (words "words.txt"))))
 
-(defn- add-word [board [dir [x y]] word]
-  (let [coord #(if (= 0 dir) [y (+ x %)] [(+ y %) x])]
-    (swap! board (fn [b]
-                   {:words (conj (b :words) (Word. dir [x y] word))
-                    :grid (reduce #(update-in %1 (coord (first %2)) (constantly (last %2)))
-                                  (b :grid)
-                                  (map-indexed vector word))}))))
+(defn- find-coord [c {:keys [direction coord pattern]}]
+  (println "--find-coord--" c " in word " pattern)
+  (let [idx (.indexOf pattern (str c) 0)
+        [x y] coord]
+    (if (= 0 direction)
+      [1 [(+ x idx) y]]
+      [0 [x (+ y idx)]])))
+
+(defn- row [board y]
+  (get (@board :grid) y))
+
+(defn- col [board x]
+  (map #(get % x) (@board :grid)))
+
+(defn- word-fit-at-intersection-at [dir x y board word]
+  (fn [idx]
+    (let [seq (if (= dir 0) (row board y) (col board x))
+          begin-seq (if (= dir 0) (- x idx) (- y idx))
+          end-seq  (- x (- (count word) idx))
+          chopped-seq (drop-last end-seq (drop begin-seq seq))
+          char-tuple-seq (partition 2 (interleave chopped-seq word))]
+      (println "--word-fit--idx: " idx word seq begin-seq end-seq chopped-seq char-tuple-seq "\n")
+      (every? #(or (= (first %) (last %))
+                   (= (first %) empty-char)) char-tuple-seq))))
+
+(defn- can-place
+  "From intersection location find if a word can be place on a board
+  returns starting location or nil"
+  [[dir [x y]] board word]
+  (let [c (get-in (@board :grid) [y x])
+        indexes (remove nil? (map-indexed #(when (= %2 c) %1) word))]
+    (println "---can-place---" c indexes )
+    (map #(identity [dir [(- x %) y]])
+     (filter (word-fit-at-intersection-at dir x y board word) indexes))))
 
 (defn- seq-to-re-str
   "Return a regex str from a seq s"
@@ -81,12 +121,13 @@
        (reduce #(str %1 (first %2) "{" (count %2) "}") "")))
 
 (defn- find-space
+  "Find a space "
   ([board start-position] (find-space board start-position (:min-length *options*)))
   ([board [dir [x y]] min-length]
    (let [extract (partial take-while #(re-matches (re-pattern (str empty-char "|" \w)) (str %)))]
      (if (= 0 dir)
-       {:dir dir :coord [x y] :pattern (seq-to-re-str (extract (drop x (get (@board :grid) y))))}
-       {:dir dir :coord [x y] :pattern (seq-to-re-str (extract (drop y (map #(get % x) (@board :grid)))))}))))
+       {:dir dir :coord [x y] :pattern (seq-to-re-str (extract (drop x (row board y))))}
+       {:dir dir :coord [x y] :pattern (seq-to-re-str (extract (drop y (col board x))))}))))
 
 (defn- random-start
   "Returns a tuple that represents:
@@ -99,3 +140,13 @@
       [direction [0 n]]
       [direction [n 0]])))
 
+(defn- filled?
+  "Predicate than tells if a board is filled
+  TODO: find better way to define a full board"
+  [board]
+  (>= (count (:words @board)) (:max-length *options*)))
+
+
+(defn generate [n user-options]
+  (binding [*options* (merge default-options user-options)]
+    (fill-board (create-board) (take n (words "words.txt")))))
--- test/crossword/core_test.clj
+++ test/crossword/core_test.clj
@@ -42,21 +42,6 @@
     (is (= ".{7}" (seq-to-re-str (map (fn [_] c/empty-char) (range 7)))))
     (is (= ".{2}a{1}.{1}e{1}.{2}" (seq-to-re-str [c/empty-char c/empty-char \a c/empty-char \e c/empty-char c/empty-char])))))
 
-;; (deftest test-find-space
-;;   (let [empty-board (create-board)
-;;         board (atom
-;;                [[c/empty-char c/empty-char \a c/empty-char \e c/empty-char c/empty-char]
-;;                 [c/empty-char c/empty-char \r c/empty-char \x c/empty-char c/empty-char]
-;;                 [\r           \e           \s \e           \c \t           \s          ]
-;;                 [c/empty-char c/empty-char \o c/empty-char \i c/empty-char c/empty-char]
-;;                 [c/empty-char c/empty-char \n c/empty-char \t c/empty-char c/empty-char]])
-;;         find-space #'c/find-space]
-;;     (testing "horizontal search"
-;;       (is (= {:dir 0 :coord [0 0] :pattern ".{7}"} (find-space empty-board [0 [0 0]] 3)))
-;;       (is (= {:dir 0 :coord [0 0] :pattern ".{2}a{1}.{1}e{1}.{2}"} (find-space board [0 [0 0]] 3))))
-;;     (testing "vertical search"
-;;       (is (= {:dir 1 :coord [0 0] :pattern ".{2}r{1}.{2}"} (find-space board [1 [0 0]] 3))))))
-
 (deftest test-add-word
   (let [add-word #'c/add-word
         board (create-board)]
@@ -78,3 +63,76 @@
               [\. \. \. \s \. \. \.]
               [\. \. \. \. \. \. \.]
               [\. \. \. \. \. \. \.]] (@board :grid))))))
+
+
+(def word-list
+  [["arc"    [0 [0 0]]]
+   ["rise"   [1 [1 0]]]
+   ["exit"   [0 [1 3]]]
+   ["rinse"  [1 [3 2]]]
+   ["sins"   [0 [3 5]]]
+   ["stains" [1 [6 0]]]
+   ["the"    [0 [1 6]]]])
+
+
+(defn- fill-board-with [words]
+  (let [board (create-board)
+        add-word #'c/add-word]
+    (doseq [[w pos] words]
+      (add-word board pos w))
+    board))
+
+(def full-board (fill-board-with word-list))
+
+(deftest test-filled?
+  (let [filled? #'c/filled?
+        board (create-board)]
+    (testing "when board empty"
+      (is (not (filled? board))))
+    (testing "when board have 7 or more words"
+      (is (filled? full-board)))))
+
+(deftest test-col
+  (is (= [\r \i \s \e \. \. \t] ((var c/col) full-board 1))))
+
+(deftest test-row
+  (is (= [\. \e \x \i \t \. \i] ((var c/row) full-board 3))))
+
+(deftest test-find-coord
+  (let [find-coord #'c/find-coord
+        w (->Word 1 [1 0] "rise")
+        [dir [x y]] (find-coord \i w)]
+    (is (= dir 0))
+    (is (= x 1))
+    (is (= y 1))))
+
+
+(deftest test-word-fit-at-intersection-at
+  (let [fun #'c/word-fit-at-intersection-at
+        board (fill-board-with (drop-last 5 word-list))
+        word "exited"
+        pred-at-idx (fun 0 1 3 board word)]
+    (is (pred-at-idx 0))
+    (is (not (pred-at-idx 4)))))
+
+(deftest test-can-place
+  (let [can-place #'c/can-place
+        board (fill-board-with (drop-last 5 word-list))
+        word "exited"
+        position [0 [1 3]]]
+    (is (= 1 (count (can-place position board word))))
+    (is (= [0 [1 3]] (first (can-place position board word))))))
+;; (deftest test-find-space
+;;   (let [empty-board (create-board)
+;;         board (atom
+;;                [[c/empty-char c/empty-char \a c/empty-char \e c/empty-char c/empty-char]
+;;                 [c/empty-char c/empty-char \r c/empty-char \x c/empty-char c/empty-char]
+;;                 [\r           \e           \s \e           \c \t           \s          ]
+;;                 [c/empty-char c/empty-char \o c/empty-char \i c/empty-char c/empty-char]
+;;                 [c/empty-char c/empty-char \n c/empty-char \t c/empty-char c/empty-char]])
+;;         find-space #'c/find-space]
+;;     (testing "horizontal search"
+;;       (is (= {:dir 0 :coord [0 0] :pattern ".{7}"} (find-space empty-board [0 [0 0]] 3)))
+;;       (is (= {:dir 0 :coord [0 0] :pattern ".{2}a{1}.{1}e{1}.{2}"} (find-space board [0 [0 0]] 3))))
+;;     (testing "vertical search"
+;;       (is (= {:dir 1 :coord [0 0] :pattern ".{2}r{1}.{2}"} (find-space board [1 [0 0]] 3))))))
