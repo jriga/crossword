5953f7511a1869f3049509f96c9081b1da5f29e3
Jerome Riga <jriga@zemis.co.uk>
2020-10-27 12:13:56 +0000

    crossword generation

--- src/crossword/core.clj
+++ src/crossword/core.clj
@@ -2,14 +2,12 @@
   (:require [clojure.java.io :as io]
             [clojure.set :as s]))
 
-(declare init-board random-start filled? find-coord can-place find-space add-word)
+(declare fill-board add-noise random-start)
 
 (defonce default-options
   {:max-length 7
    :min-length 3
    :exclude-pattern #"\s|\d|\W"})
-
-(defonce ^:dynamic *options* (merge default-options {}))
 
 (defonce empty-char \.)
 
@@ -26,11 +24,8 @@
   - exclude-pattern, regex that exclude certain words
 
   TODO: look into transducers for this function instead of the threading macro"
-  ([source] (words source
-                   (:max-length *options*)
-                   (:min-length *options*)
-                   (:exclude-pattern *options*)))
-  ([source max-length min-length exclude-pattern]
+  ([source] (words source default-options))
+  ([source {:keys [max-length min-length exclude-pattern]}]
    (with-open [rdr (io/reader (io/resource source))]
      (->> (line-seq rdr)
           (remove #(or (re-find exclude-pattern %)
@@ -44,90 +39,114 @@
   "Creates a crossword board
   args:
   - n, int for the size of the square board"
-  ([] (create-board (:max-length *options*)))
-  ([n] (atom
-        {:words []
-         :grid (mapv #(into [] %) (partition n (repeat (* n n) empty-char)))})))
+  ([] (create-board (:max-length default-options)))
+  ([n]
+   {:words []
+    :grid (mapv #(into [] %) (partition n (repeat (* n n) empty-char)))}))
 
-(def ^:private not-nil? (complement nil?))
+(defn generate
+  ([]  (generate 100))
+  ([n] (generate n {}))
+  ([n user-options]
+   (let [options (merge default-options user-options)
+         board (create-board (:max-length options))]
+     (-> board
+         (fill-board
+                 (take n (words "words.txt" options))
+                 (:start options (random-start board)))
+         add-noise))))
+
+(defn- present? [board word]
+  (some #(= word %) (map :pattern (:words board))))
+
 (defn- add-word [board [dir [x y]] word]
-  (when (and (not-nil? dir) (not-nil? x) (not-nil? y))
-    (let [coord #(if (= 0 dir) [y (+ x %)] [(+ y %) x])]
-      (swap! board (fn [b]
-                     {:words (conj (b :words) (Word. dir [x y] word))
-                      :grid (reduce #(update-in %1 (coord (first %2)) (constantly (last %2)))
-                                    (b :grid)
-                                    (map-indexed vector word))})))))
+  "TODO: add a stop # mark at each end of word unless end is board's edge"
+  (let [coord #(if (= 0 dir) [y (+ x %)] [(+ y %) x])]
+    (if (present? board word)
+      board
+      {:words (conj (board :words) (Word. dir [x y] word))
+       :grid (reduce #(update-in %1 (coord (first %2)) (constantly (last %2)))
+                     (:grid board)
+                     (map-indexed vector word))})))
 
-(defn fill-board
+(defn- build-slots [c board-word word]
+  "retuns a sequence of potential position for word based on one
+   intersection of word on the board board-word and word"
+  (let [[cx cy] (:coord board-word)]
+    (for [[x _] (filter #(= c (last %)) (map-indexed vector (:pattern board-word)))
+          [y _] (filter #(= c (last %)) (map-indexed vector word))]
+      (do
+        ;; (print "--build-slots-- char: " c " bword: " (:pattern board-word) " word: " word)
+        (if (= (:direction board-word) 0)
+           [1 [(+ cx x) (- cy y)]]
+           [0 [(- cx y) (+ cy x)]])))))
+
+(defn- find-slot [word]
+  "returns fn that will returns all the potential positions for word based on all the
+   intersections with a word on the board"
+  (fn [{:keys [bword inter]}]
+    (mapcat #(build-slots % bword word) inter)))
+
+(defn- row [board y]
+  "returns row at y on board"
+  (get (:grid board) y))
+
+(defn- col [board x]
+  "returns column at x on board"
+  (map #(get % x) (:grid board)))
+
+(defn- collision-free?
+  "predicate retuns true word collides with existing words"
+  [board [dir [x y]] word]
+  (let [seq (if (= dir 0) (row board y) (col board x))
+        begin-seq (if (= dir 0) x y)
+        char-tuple-seq (partition 2 (interleave (drop begin-seq seq) word))]
+    ;; (println "check collisions: " [dir [x y]] word char-tuple-seq )
+    (every? #(or (= (first %) (last %))
+                 (= (first %) empty-char)) char-tuple-seq)))
+
+(defn- invalid-slot [board word]
+  "returns a predicate fn that returns true when proposed
+   position for word is invalid when:
+   - start x,y is out of bounds
+   - horizontal propositon results in last char out of bounds
+   - vertical propositon results in last char out of bounds
+   - placing word would interfere with existing words"
+  (let [max (count (:grid board))
+        size (count word)]
+    (fn [[dir [x y] :as position]]
+      ;; (println "validation: (neg? x): "(neg? x) " (neg? y):" (neg? y)
+      ;;        "\n (and (= dir 0) (> (+ x size) max)):" (and (= dir 0) (> (+ x size) max)) " " (+ x size) max
+      ;;        "\n (and (= dir 1) (> (+ y size) max)):" (and (= dir 1) (> (+ y size) max)) " " (+ y size) max
+      ;;        "\n (not (collision-free? board position word)): ")
+      (or (neg? x)
+          (neg? y)
+          (and (= dir 0) (> (+ x size) max))
+          (and (= dir 1) (> (+ y size) max))
+          (not (collision-free? board position word))))))
+
+(defn- find-position [board word]
+  "Returns the first valid slot for word on the board"
+  (let [set-word (set word)
+        xform (comp (map #(hash-map :bword % :inter (s/intersection set-word (set (:pattern %)))))
+                    (remove #(empty? (:inter %)))
+                    (mapcat (find-slot word))
+                    ;; (map #(do (print "   position: " % " -- \n") %) )
+                    (remove (invalid-slot board word)))]
+    (first (sequence xform (reverse (:words board))))))
+
+(defn- place-word [board word]
+  (if-let [position (find-position board word)]
+    (add-word board position word)
+    board))
+
+(defn- fill-board
   "Fills in given board with the words provided"
   ([board word-seq] (fill-board board word-seq (random-start board)))
   ([board word-seq position]
    (let [sorted-word (sort-by count > word-seq)
-         _ (add-word board position (first sorted-word))]
-     (doseq [w (drop 1 sorted-word)]
-       (when-let [intersections (s/intersection (set w) (set (:pattern (last (:words @board)))))]
-         (println "Current word: " w "intersections with " (:pattern (last (:words @board))) "is " intersections)
-         (add-word board
-                   (first (map  #(-> %
-                                     (find-coord (last (:words @board)))
-                                     (can-place board w)
-                                     (first))
-                                intersections))
-                   w))))))
-
-
-(defn- find-coord [c {:keys [direction coord pattern]}]
-  (println "--find-coord--" c " in word " pattern)
-  (let [idx (.indexOf pattern (str c) 0)
-        [x y] coord]
-    (if (= 0 direction)
-      [1 [(+ x idx) y]]
-      [0 [x (+ y idx)]])))
-
-(defn- row [board y]
-  (get (@board :grid) y))
-
-(defn- col [board x]
-  (map #(get % x) (@board :grid)))
-
-(defn- word-fit-at-intersection-at [dir x y board word]
-  (fn [idx]
-    (let [seq (if (= dir 0) (row board y) (col board x))
-          begin-seq (if (= dir 0) (- x idx) (- y idx))
-          end-seq  (- x (- (count word) idx))
-          chopped-seq (drop-last end-seq (drop begin-seq seq))
-          char-tuple-seq (partition 2 (interleave chopped-seq word))]
-      (println "--word-fit--idx: " idx word seq begin-seq end-seq chopped-seq char-tuple-seq "\n")
-      (every? #(or (= (first %) (last %))
-                   (= (first %) empty-char)) char-tuple-seq))))
-
-(defn- can-place
-  "From intersection location find if a word can be place on a board
-  returns starting location or nil"
-  [[dir [x y]] board word]
-  (let [c (get-in (@board :grid) [y x])
-        indexes (remove nil? (map-indexed #(when (= %2 c) %1) word))]
-    (println "---can-place---" c indexes )
-    (map #(identity [dir [(- x %) y]])
-     (filter (word-fit-at-intersection-at dir x y board word) indexes))))
-
-(defn- seq-to-re-str
-  "Return a regex str from a seq s"
-  [s]
-  (->> s
-       (map #(str (if (= empty-char %) "." %)))
-       (partition-by #(re-find (re-pattern (str empty-char)) %))
-       (reduce #(str %1 (first %2) "{" (count %2) "}") "")))
-
-(defn- find-space
-  "Find a space "
-  ([board start-position] (find-space board start-position (:min-length *options*)))
-  ([board [dir [x y]] min-length]
-   (let [extract (partial take-while #(re-matches (re-pattern (str empty-char "|" \w)) (str %)))]
-     (if (= 0 dir)
-       {:dir dir :coord [x y] :pattern (seq-to-re-str (extract (drop x (row board y))))}
-       {:dir dir :coord [x y] :pattern (seq-to-re-str (extract (drop y (col board x))))}))))
+         init-board (add-word board position (first sorted-word))]
+     (reduce place-word init-board word-seq))))
 
 (defn- random-start
   "Returns a tuple that represents:
@@ -135,20 +154,15 @@
   - position, on the board"
   [board]
   (let [direction (rand-int 2)
-        n (rand-int (count (@board :grid)))]
+        n (rand-int (count (:grid board)))]
     (if (= 0 direction)
       [direction [0 n]]
       [direction [n 0]])))
 
-(defn- filled?
-  "Predicate than tells if a board is filled
-  TODO: find better way to define a full board"
-  [board]
-  (>= (count (:words @board)) (:max-length *options*)))
-
-
-(defn generate [n user-options]
-  (binding [*options* (merge default-options user-options)]
-    (let [board (create-board)]
-      (fill-board board (take n (words "words.txt")))
-      board)))
+(defn- add-noise [board]
+  "Returns a board with empty-char replaced by random alphabet char"
+  (let [chars (map char (range (int \a) (+ (int \a) 26)))]
+    {:words (:words board)
+     :grid (map
+            (fn [row] (map #(if (= % empty-char) (rand-nth chars) %) row))
+            (:grid board))}))
--- test/crossword/core_test.clj
+++ test/crossword/core_test.clj
@@ -98,30 +98,70 @@
       (is (= [\. \e \x \i \t \. \i] ((var c/row) full-board 3))))))
 
 
-;; (deftest test-build-slots
-;;   (let [build-slots #'c/build-slots
-;;         vertical-word (c/->Word 1 [3 1] "nine")
-;;         horizontal-word (c/->Word 0 [3 1] "nine")
-;;         word "netten"]
-;;     (ip (= [[0 [3 1]]
-;;             [0 [-2 1]]
-;;             [0 [3 3]]
-;;             [0 [-2 3]]]
-;;            (build-slots \n vertical-word word)))
-;;     (is (= [0 [2 4]]
-;;            [0 [-1 4]]
-;;            (build-slots \e vertical-word word)))
-;;     (is (= [1 [6 0]]
-;;            [1 [6 -3]]
-;;            (build-slots \e horizontal-word word)))))
+(deftest test-build-slots
+  (let [build-slots #'c/build-slots
+        vertical-word (c/->Word 1 [3 1] "nine")
+        horizontal-word (c/->Word 0 [3 1] "nine")
+        word "netten"]
+    (is (= [[0 [3 1]]
+            [0 [-2 1]]
+            [0 [3 3]]
+            [0 [-2 3]]]
+           (build-slots \n vertical-word word)))
+    (is (= [[0 [2 4]]
+            [0 [-1 4]]]
+           (build-slots \e vertical-word word)))
+    (is (= [[1 [6 0]]
+            [1 [6 -3]]]
+           (build-slots \e horizontal-word word)))))
 
-;; (deftest test-find-slot
-;;   (let [find-slot #'c/find-slot
-;;         vertical-word (->Word 1 [3 1] "nine")
-;;         horizontal-word (->Word 0 [3 1] "nine")
-;;         word "netten"]
-;;     (is (= [[0 [3 1]]
-;;             [0 [-2 1]]
-;;             [0 [3 3]]
-;;             [0 [-2 3]]]
-;;            ((find-slot word) {:bword vertical-word :inter #{\n \e}})))))
+(deftest test-find-slot
+  (let [find-slot #'c/find-slot
+        vertical-word (->Word 1 [3 1] "nine")
+        horizontal-word (->Word 0 [3 1] "nine")
+        word "netten"]
+    (is (= [[0 [2 4]]
+            [0 [-1 4]]
+            [0 [3 1]]
+            [0 [-2 1]]
+            [0 [3 3]]
+            [0 [-2 3]]]
+           ((find-slot word) {:bword vertical-word :inter #{\n \e}})))))
+
+(deftest test-invalid-slot
+  (let [invalid-slot #'c/invalid-slot
+        board (fill-board-with (drop-last 3 word-list))]
+    (testing "start position out of bounds"
+      (is ((invalid-slot board "tether") [0 [-1 6]])))
+    (testing "end position out of bounds"
+      (is ((invalid-slot board "sinister") [0 [3 5]])))
+    (testing "collides with other existing word"
+      (is ((invalid-slot board "car") [0 [1 2]])))))
+
+(deftest test-find-position
+  (let [find-position #'c/find-position
+        board (fill-board-with (drop-last 3 word-list))
+        actual (find-position board "sins")]
+    (testing "returns one of valid positions"
+      (is (some #(= % actual) [[0 [1 4]] [0 [3 5]] [0 [0 5]] [0 [0 1]]])))
+    (testing "returns nil otherwise"
+      (is (= nil (find-position board "sinister"))))))
+
+(deftest test-place-word
+  (let [place-word #'c/place-word
+        board (fill-board-with (drop-last 3 word-list))]
+    (testing "add word when a position is found"
+      (is (some #(= "sins" (:pattern %)) (:words (place-word board "sins")))))
+    (testing "returns original board when no position found"
+      (is (empty? (filter #(= "sinister" (:pattern %)) (:words (place-word board "sinister"))))))))
+
+(deftest test-fill-board
+  (let [fill-board #'c/fill-board
+        board (create-board)]
+    (is (not (empty? (:words (fill-board board ["arcs" "sins" "stains"])))))))
+
+(deftest test-add-noise
+  (let [full-board (fill-board-with word-list)
+        actual ((var c/add-noise) full-board)]
+    (doseq [x (range 7) y (range 7)]
+      (is (not= c/empty-char (get-in actual [x y]))))))
