0c20d21f8ff7acc3eb989179f50400005466c0fd
Jerome Riga <jriga@zemis.co.uk>
2020-10-28 19:10:10 +0000

    pass correct js options

--- src/crossword/core.cljs
+++ src/crossword/core.cljs
@@ -1,9 +1,13 @@
 (ns crossword.core
   (:require [crossword.generator :as cg]))
 
+(defn- parse-js-options [o]
+  (into {} (for [[k v] (js->clj o)]
+             [(keyword k) v])))
+
 (defn ^:export create 
   ([words] (create words {}))
   ([words user-options]
-   (let [options (merge cg/default-options user-options)]
-     (clj->js (cg/generate words (js->clj options))))))
+   (let [options (merge cg/default-options (parse-js-options user-options))]
+     (clj->js (cg/generate words options)))))
 
