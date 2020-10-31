a458aa89e3750507d9956e59d6e8445fcc8f2be3
Jerome Riga <jriga@zemis.co.uk>
2020-10-31 16:49:18 +0000

    add heroku setting

--- project.clj
+++ project.clj
@@ -3,11 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-cljsbuild "1.1.8"]]
+  :plugins [[lein-cljsbuild "1.1.8"]
+            [environ/environ.lein "0.3.1"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.741"]]
+                 [org.clojure/clojurescript "1.10.741"]
+                 [compojure "1.6.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [environ "1.1.0"]]
+  :min-lein-version "2.0.0"
   :repl-options {:init-ns crossword.core}
-  :profiles {:dev {:resource-paths ["test/resources"]}}
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "crossword-standalone.jar"
+  :profiles 
+  {:dev {:resource-paths ["test/resources"]}
+   :production {:env {:production true}}}
   :cljsbuild
   {:test-command
    {"node" ["node" :node-runner "target/main.js"]}
---
+++ src/crossword/web.clj
@@ -0,0 +1,47 @@
+(ns crossword.web
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]
+            [crossword.core :as cw]
+            [clojure.data.json :as json]
+            [clojure.string :as str]
+            [clojure.java.io :as io])
+  (:gen-class))
+
+(defn request-words [req]
+  {:status  200
+   :headers {"Content-Type" "application/json"}
+   :body    (let [max (Integer/parseInt (get-in req [:params :max] "7"))
+                  min (Integer/parseInt (get-in req [:params :min] "3"))
+                  limit (Integer/parseInt (get-in req [:params :limit] "10"))]
+              (-> (take limit
+                        (cw/words "words.txt" 
+                                  {:max-length max
+                                   :min-length min
+                                   :exclude-pattern #"\s|\d|\W"}))
+                  (json/write-str)
+                  str))})
+
+(defn landing [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Words World"})
+
+(defroutes app
+  (GET "/" [] (io/resource "public/index.html"))
+  (GET "/request" [] request-words)
+  (route/resources "/")
+  (ANY "*" []
+       (route/not-found "Bad request")))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))
+
